6d6d1df69032e1f65491f56d03e4bfc0a8edc94e
ara-bara <archomoris2510@gmail.com>
2023-03-17 17:53:43 +0200

    slaider 25%

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+/*burger menu*/
 let menu__button = document.querySelector('.menu__button');
 let menu__burger = document.querySelector('.menu__burger');
 menu__button.addEventListener('click', function(){
     menu__button.classList.toggle('active');
     menu__burger.classList.toggle('active');
 })
+
+/*slider*/
+
+
+$(document).ready(function(){
+    $('.slider').slick();
+});
